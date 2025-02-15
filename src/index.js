542dbc99c18c52c44327e5b9bd29a73423a98eda
dharshan29 <dharshangowda2000@gmail.com>
2025-02-15 21:56:26 +0530

    cors

--- src/index.js
+++ src/index.js
@@ -14,7 +14,12 @@ const PORT = process.env.PORT || 5200;
 const PATH_NAME = process.env.PATH_NAME || "/api";
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.options('*', cors());
